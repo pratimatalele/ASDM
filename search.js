75919d42977255ed54ef98233fff17fe1b9b3700
pratimatalele <50071559+pratimatalele@users.noreply.github.com>
2019-04-29 17:41:58 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,15 @@
+var arr=[10,20,30,40];
+var flag=0;
+for(var i=0;i<arr.length;i++)
+{
+	if(arr[i]==69)
+	{
+		console.log("found");
+		flag=1;
+	}
+}
+
+if(flag==0)
+{
+	console.log("not found");
+}
